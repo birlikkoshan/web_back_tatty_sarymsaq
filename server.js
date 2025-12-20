360c2d175657b2d65414a7127fc739c5ef6d0973
Phon2 <gadclash7@gmail.com>
2025-12-20 15:05:35 +0500

    feat:chatgpt ahh code

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const app = express();
 app.use(express.static('public'));
+app.use(express.static('views'));
+app.use(express.urlencoded({extended:true}));
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
+
+app.post('/contact', (req, res) => {
+    console.log(req.body);
+    res.send(`<h2>Thanks, ${req.body.name}! Your message has been received.</h2>`);
+});
+
+app.use((req,res) => {
+    res.status(404).sendFile(__dirname + '/views/not_found.html')
+})
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
